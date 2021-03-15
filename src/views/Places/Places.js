b64694312c694890fa4f988ed4f8b5b5c3e039d9
Luciemzt <lucie.mzt@gmail.com>
2021-03-15 15:59:49 +0100

    dione

--- src/views/Places/Places.js
+++ src/views/Places/Places.js
@@ -6,7 +6,7 @@ import { usePlaces } from "../../context/PlaceContext";
 
 
 function Places() {
-const {places, setPlaces, getAllPlaces, getPlace} = usePlaces()
+const {places, getAllPlaces} = usePlaces()
 
   React.useEffect(()=>{
     getAllPlaces()
@@ -15,9 +15,9 @@ const {places, setPlaces, getAllPlaces, getPlace} = usePlaces()
     return (
       <div>
             {places.map((place) => (
-            <div key={places._id}>
-              <h5>{places.name}</h5>
-              <p>{places.type}</p>
+            <div key={place._id}>
+              <h5>{place.name}</h5>
+              <p>{place.type}</p>
               <Link to={"/place/${place._id}"}>View More</Link>
             </div> 
           ))}
